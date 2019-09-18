365238d4a94e9821cfea0c76aaca389ff350dff8
Neal Kruis <neal.kruis@bigladdersoftware.com>
2019-09-18 15:48:23 -0600

    Resolve schema refs on local file system.

--- schema/TestValidate.py
+++ schema/TestValidate.py
@@ -1,8 +1,10 @@
 import json
-from jsonschema import validate
+import jsonschema
+import os
 
+resolver = jsonschema.RefResolver('file://%s/' % os.path.abspath(os.path.dirname(__file__)), None)
 with open("ASHRAE205.schema.json", "r") as read_file:
     schema = json.load(read_file)
     with open("RS0001ExampleFile.json", "r") as test_file:
         file = json.load(test_file)
-        validate(instance=file, schema=schema)
+        jsonschema.validate(instance=file, schema=schema, resolver=resolver)
