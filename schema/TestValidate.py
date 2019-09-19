23101a55cab36c8bbd84bc6f9b5a65535e2f7645
Neal Kruis <neal.kruis@bigladdersoftware.com>
2019-09-19 09:57:26 -0600

    Use posix path for json schema ref resolution.

--- schema/TestValidate.py
+++ schema/TestValidate.py
@@ -1,8 +1,8 @@
 import json
 import jsonschema
-import os
+import posixpath
 
-resolver = jsonschema.RefResolver('file://%s/' % os.path.abspath(os.path.dirname(__file__)), None)
+resolver = jsonschema.RefResolver('file://%s/' % posixpath.abspath(posixpath.dirname(__file__)), None)
 with open("ASHRAE205.schema.json", "r") as read_file:
     schema = json.load(read_file)
     with open("RS0001ExampleFile.json", "r") as test_file:
