b5541d021f9ec01749494cab4608be3e5cc1f611
Neal Kruis <neal.kruis@bigladdersoftware.com>
2019-12-11 15:57:29 -0700

    Clean up and minor fixes.

--- web/web-content.py
+++ web/web-content.py
@@ -7,6 +7,8 @@ from distutils.dir_util import copy_tree
 from jinja2 import Environment, FileSystemLoader
 import tk205
 
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
 def get_title_and_description(json_file, path):
     """
     Open an ASHRAE205 json schema or example file and return the title and description.
@@ -49,43 +51,43 @@ def generate_page(env, template_name, file_name, destination, headline, content)
             nav = file_name,
             headline = headline,
             content = content
-        )) 
+        ))
     f.close()
 
 
-def create_files(rss, open205_dir):
+def create_files(web_dir):
     """
-    Create or replace the example and template files inside of open205.github.io 
+    Create or replace the example and template files inside of open205.github.io
     """
-    assets_dir = os.path.join(open205_dir, 'assets')
+    assets_dir = os.path.join(web_dir, 'assets')
     if not os.path.isdir(assets_dir):
         os.mkdir(assets_dir)
 
-    examples_dir = os.path.join(open205_dir, 'assets', 'examples')
+    examples_dir = os.path.join(assets_dir, 'examples')
     if not os.path.isdir(examples_dir):
         os.mkdir(examples_dir)
 
-    json_dir = os.path.join(open205_dir, 'assets', 'examples', 'json')
+    json_dir = os.path.join(examples_dir, 'json')
     if not os.path.isdir(json_dir):
         os.mkdir(json_dir)
 
-    cbor_dir = os.path.join(open205_dir, 'assets', 'examples', 'cbor')
+    cbor_dir = os.path.join(examples_dir, 'cbor')
     if not os.path.isdir(cbor_dir):
         os.mkdir(cbor_dir)
 
-    xlsx_dir = os.path.join(open205_dir, 'assets', 'examples', 'xlsx')
+    xlsx_dir = os.path.join(examples_dir, 'xlsx')
     if not os.path.isdir(xlsx_dir):
         os.mkdir(xlsx_dir)
 
-    yaml_dir = os.path.join(open205_dir, 'assets', 'examples', 'yaml')
+    yaml_dir = os.path.join(examples_dir, 'yaml')
     if not os.path.isdir(yaml_dir):
         os.mkdir(yaml_dir)
 
-    templates_dir = os.path.join(open205_dir, 'assets', 'templates')
+    templates_dir = os.path.join(assets_dir, 'templates')
     if not os.path.isdir(templates_dir):
         os.mkdir(templates_dir)
 
-    schema_dir = os.path.join(open205_dir, 'assets', 'schema')
+    schema_dir = os.path.join(assets_dir, 'schema')
     if not os.path.isdir(schema_dir):
         os.mkdir(schema_dir)
 
@@ -97,94 +99,88 @@ def create_files(rss, open205_dir):
 
     # xlsx_template_creation
     tk205.file_io.clear_directory(templates_dir)
-    for rs in rss:
-        file_name_components = [rs["RS"]]
-        if rs["file-name-suffix"]:
-            file_name_components.append(rs["file-name-suffix"])
+    template_content = tk205.load(os.path.join(root_dir, "..", "config", "templates.json"))
+    for template in template_content:
+        file_name_components = [template["RS"]]
+        if template["file-name-suffix"]:
+            file_name_components.append(template["file-name-suffix"])
         file_name_components.append("template.a205.xlsx")
         file_name = '-'.join(file_name_components)
-        tk205.template(rs["RS"],os.path.join(templates_dir,file_name), **rs["keywords"])
+        tk205.template(template["RS"],os.path.join(templates_dir,file_name), **template["keywords"])
 
 
-def clone(root_dir):
+def clone():
     """
     Clone open205.github.io website repository into the build directory.
     """
-    open205_destination = os.path.join(root_dir, '..', 'build')
-    if not os.path.isdir(open205_destination):
-        os.mkdir(open205_destination)
-    open205_dir = os.path.join(open205_destination, 'open205.github.io')
-    if not os.path.isdir(open205_dir):
-        git.Repo.clone_from("https://github.com/open205/open205.github.io.git", open205_dir, branch='master')
-    open205_assets_dir = os.path.join(open205_dir, 'assets')
-    if not os.path.isdir(open205_assets_dir):
-        os.mkdir(open205_assets_dir)
-    return open205_dir
+    build_dir = os.path.join(root_dir, '..', 'build')
+    if not os.path.isdir(build_dir):
+        os.mkdir(build_dir)
+    web_dir = os.path.join(build_dir, 'web')
+    if not os.path.isdir(web_dir):
+        git.Repo.clone_from("https://github.com/open205/open205.github.io.git", web_dir, branch='master')
+    assets_dir = os.path.join(web_dir, 'assets')
+    if not os.path.isdir(assets_dir):
+        os.mkdir(assets_dir)
+    return web_dir
 
 
-def generate(params, root, open205_dir):
+def generate(web_dir):
     """
     Generate all content for the open205.github.io repository
     """
     # Setup for Jinja2
-    jinja_templates_dir = os.path.join(root, 'templates')
+    jinja_templates_dir = os.path.join(root_dir, 'templates')
     env = Environment( loader = FileSystemLoader(jinja_templates_dir) )
 
-    schema_directory = os.path.join(root, "..", "schema-205", "schema")
-    examples_directory = os.path.join(open205_dir, "assets", "examples")
-    templates_directory = os.path.join(open205_dir, "assets", "templates")
+    schema_directory = os.path.join(root_dir, "..", "schema-205", "schema")
+    examples_directory = os.path.join(web_dir, "assets", "examples")
+    templates_directory = os.path.join(web_dir, "assets", "templates")
 
     schema_dictionary = get_directory_structure(schema_directory)
     examples_dictionary = get_directory_structure(examples_directory)
     templates_dictionary = get_directory_structure(templates_directory)
 
     # Create schema.html
-    schema_page_data = OrderedDict() 
+    schema_page_data = OrderedDict()
     for schema_file in sorted(schema_dictionary):
         title, description = get_title_and_description(schema_file, schema_directory)
-        schema_page_data[title] = {'title':title, 'description':description, 'schema_file':schema_file}
-    generate_page(env, 'schema_template.html', 'schema.html', open205_dir, 'JSON Schema (Normative)', schema_page_data)
+        schema_page_data[title] = {'title': title, 'description': description, 'schema_file': schema_file}
+    generate_page(env, 'schema_template.html', 'schema.html', web_dir, 'JSON Schema (Normative)', schema_page_data)
 
     # Create examples.html
     examples_page_data = OrderedDict()
     for index, example_file in enumerate(sorted(examples_dictionary['json'])):
         file_list = []
+        title, description = get_title_and_description(example_file, os.path.join(examples_directory, "json"))
+
         base_name = os.path.splitext(example_file)[0]
         for keys, example_types in examples_dictionary.items():
             for example in example_types:
                 if base_name in example:
                     file_list.append(example)
-        examples_page_data[example_file]={'title':params[index]['RS'], 'description':params[index]['description'], 'file_list':file_list}
-    generate_page(env, 'examples_template.html', 'examples.html', open205_dir, 'Example Files', examples_page_data)
+        examples_page_data[example_file]={'title': title, 'description': description, 'file_list': file_list}
+    generate_page(env, 'examples_template.html', 'examples.html', web_dir, 'Example Files', examples_page_data)
 
     # Create templates.html
+    template_content = tk205.load(os.path.join(root_dir, "..", "config", "templates.json"))
+
     templates_page_data = OrderedDict()
     for index, template_file in enumerate(templates_dictionary):
-        templates_page_data[template_file] = {'title':params[index]['RS'], 'description':params[index]['description'], 'template_file':template_file}
-    generate_page(env, 'templates_template.html', 'templates.html', open205_dir, 'XLSX Templates', templates_page_data)
+        templates_page_data[template_file] = {'title':template_content[index]['RS'], 'description':template_content[index]['description'], 'template_file':template_file}
+    generate_page(env, 'templates_template.html', 'templates.html', web_dir, 'XLSX Templates', templates_page_data)
 
     # Create index.html AKA about page
-    generate_page(env, 'about_template.html', 'index.html', open205_dir, '', None)
+    generate_page(env, 'about_template.html', 'index.html', web_dir, '', None)
 
     # Create toolkit205.html
-    generate_page(env, 'tk205_template.html', 'tk205.html', open205_dir, '', None)
+    generate_page(env, 'tk205_template.html', 'tk205.html', web_dir, '', None)
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Param file required.")
-        exit()
-
-    paramfile = sys.argv[1]
-    if (tk205.get_extension(paramfile).lower() == '.json'):
-        with open(paramfile) as json_file:
-            params = (json.load(json_file))
-        json_file.close()
-    root = os.path.dirname(os.path.abspath(__file__))
-
-    open205_dir = clone(root)
-    create_files(params, open205_dir)
-    generate(params, root, open205_dir)
+    web_dir = clone()
+    create_files(web_dir)
+    generate(web_dir)
 
 
 if __name__ == '__main__':
