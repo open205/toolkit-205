be4d7e7dc2f6e4c3b2acee6ed7613bbebdff9d98
Neal Kruis <neal.kruis@bigladdersoftware.com>
2020-05-22 16:55:25 -0600

    Clean up unit test name.

--- test/test_tk205_unit.py
+++ test/test_tk205_unit.py
@@ -97,7 +97,7 @@ def test_get_schema_rs_title():
     title = schema.get_rs_title('RS0001')
     assert(title == "Liquid-Cooled Chiller")
 
-def test_objects_near():
+def test_objects_near_equal():
     # Numbers
     assert(tk205.util.objects_near_equal(3.0,3.0001,abs_tol=0.01))
     assert(not tk205.util.objects_near_equal(3.0,3.0001,abs_tol=0.0000001))
