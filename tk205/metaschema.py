4b94b4463f477d5d856e5d899cbe78f9ae524de3
tanaya-bigladder <57679220+tanaya-bigladder@users.noreply.github.com>
2020-06-23 17:06:29 -0600

    Clean up Data Elements collection; add Performance Map, Variables groups.

--- tk205/metaschema.py
+++ tk205/metaschema.py
@@ -26,8 +26,7 @@ class DataGroup:
 
     def add_data_group(self, group_name, group_subdict):
         elements = {'type': 'object',
-                    'properties' : dict(),
-                    'additionalProperties' : False}
+                    'properties' : dict()}
         required = list()
         for e in group_subdict:
             element = group_subdict[e]
@@ -42,6 +41,7 @@ class DataGroup:
             if 'Required' in element:
                 required.append(e)
             elements['required'] = required
+            elements['additionalProperties'] = False
 
         return {group_name : elements}
 
@@ -49,41 +49,50 @@ class DataGroup:
     def _create_type_entry(self, parent_dict, target_dict):
         try:
             # If the type is an array, extract the surrounding [] first (using non-greedy qualifier "?")
-            m = re.match(r'\[(.*?)\]', parent_dict['Data Type'])
+            m = re.findall(r'\[(.*?)\]', parent_dict['Data Type'])
             if m:
                 target_dict['type'] = 'array'
                 target_dict['items'] = dict()
-                target_dict['items']['type'] = self._get_simple_type(m.group(1))
-                self._get_simple_minmax(parent_dict['Range'], target_dict['items'])
-                if len(m.groups()) > 1:
-                    target_dict['minItems'] = int(m.group(2))
-                    target_dict['maxItems'] = int(m.group(2))
+                k, v = self._get_simple_type(m[0])
+                target_dict['items'][k] = v
+                if 'Range' in parent_dict:
+                    self._get_simple_minmax(parent_dict['Range'], target_dict['items'])
+                if len(m) > 1:
+                    target_dict['minItems'] = int(m[1])
+                    target_dict['maxItems'] = int(m[1])
                 else:
                     target_dict['minItems'] = 1
             else:
-                target_dict['type'] = self._get_simple_type(parent_dict['Data Type'])
+                k, v = self._get_simple_type(parent_dict['Data Type'])
+                target_dict[k] = v
                 self._get_simple_minmax(parent_dict['Range'], target_dict)
-        except KeyError:
-            # some good error msg for two keys, esp. Range
-            pass
+        except KeyError as ke:
+            print('KeyError; no key exists called', ke)
+            # some better error msg for two keys, esp. Range
 
 
     def _get_simple_type(self, type_str):
         m = re.match(r'\{(.*)\}', type_str)
-        if m: # Enum type
+        if m: # Definition type
             internal_type = m.group(1)
-            return internal_type
+            return ('$ref', internal_type)
+        else:
+            m = re.match(r'\<(.*)\>', type_str)
+            if m: # Enum  type
+                internal_type = m.group(1)
+                return ('$ref', internal_type)
         if type_str == 'String':
-            return 'string'
+            return ('type', 'string')
         elif type_str == 'Numeric':
-            return 'number'
+            return ('type', 'number')
         elif type_str == 'Integer':
-            return 'integer'
+            return ('type', 'integer')
         elif type_str == 'Boolean':
-            return 'boolean'
+            return ('type', 'boolean')
         else:
             # Validation error
-            return None
+            print(type_str)
+            return (None, None)
 
 
     def _get_simple_minmax(self, range_str, target_dict):
@@ -151,9 +160,24 @@ class JSON_translator:
                 sch = {**sch, **(self._process_enumeration(base_level_tag))}
             if ('Object Type' in self._contents[base_level_tag] and 
                 self._contents[base_level_tag]['Object Type'] == 'Data Group'):
-                #sch = {**sch, **(self._process_datagroup(base_level_tag))}
                 dg = DataGroup(base_level_tag)
-                sch = {**sch, **(dg.add_data_group(base_level_tag, self._contents[base_level_tag]['Data Elements']))}
+                sch = {**sch, **(dg.add_data_group(base_level_tag, 
+                                                   self._contents[base_level_tag]['Data Elements']))}
+            if ('Object Type' in self._contents[base_level_tag] and 
+                self._contents[base_level_tag]['Object Type'] == 'Performance Map'):
+                dg = DataGroup(base_level_tag)
+                sch = {**sch, **(dg.add_data_group(base_level_tag, 
+                                                   self._contents[base_level_tag]['Data Elements']))}
+            if ('Object Type' in self._contents[base_level_tag] and 
+                self._contents[base_level_tag]['Object Type'] == 'Grid Variables'):
+                dg = DataGroup(base_level_tag)
+                sch = {**sch, **(dg.add_data_group(base_level_tag, 
+                                                   self._contents[base_level_tag]['Data Elements']))}
+            if ('Object Type' in self._contents[base_level_tag] and 
+                self._contents[base_level_tag]['Object Type'] == 'Lookup Variables'):
+                dg = DataGroup(base_level_tag)
+                sch = {**sch, **(dg.add_data_group(base_level_tag, 
+                                                   self._contents[base_level_tag]['Data Elements']))}
         self._schema['definitions'] = sch
         return self._schema
 
@@ -170,23 +194,6 @@ class JSON_translator:
             except TypeError: # key's value is None
                 definition.add_enumerator(key)
         return definition.create_dictionary_entry()
-
-    def _process_datagroup(self, name_key):
-        ''' Collect all Data Elements in a DataGroup block. '''
-        datagroup = DataGroup(name_key)
-        elements = self._contents[name_key]['Data Elements']
-        for key in elements: # keys are actual property names
-            try:
-                datagroup.add_data_element(key, 
-                    description=elements[key]['Description'] if 'Description' in elements[key] else None,
-                    data_type=elements[key]['Data Type'] if 'Data Type' in elements[key] else None,
-                    notes=elements[key]['Notes'] if 'Notes' in elements[key] else None,
-                    required=elements[key]['Required'] if 'Required' in elements[key] else None,
-                    units=elements[key]['Units'] if 'Units' in elements[key] else None,
-                    range=elements[key]['Range'] if 'Range' in elements[key] else None)
-            except TypeError:
-                pass 
-        return datagroup.create_dictionary_entry()
 
 
 # -------------------------------------------------------------------------------------------------
