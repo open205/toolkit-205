f2e7028461150f4f47d583a2d4644f0da310e7f8
tanaya-bigladder <57679220+tanaya-bigladder@users.noreply.github.com>
2020-07-01 11:49:24 -0600

    Clean up type-collection. Consolidate entry ordering.

--- tk205/commonschema.py
+++ tk205/commonschema.py
@@ -10,8 +10,9 @@ import sys
 # -------------------------------------------------------------------------------------------------
 class DataGroup:
 
-    def __init__(self, name, ref_list=None):
+    def __init__(self, name, type_list, ref_list=None):
         self._name = name
+        self._types = type_list
         self._refs = ref_list
 
 
@@ -31,9 +32,9 @@ class DataGroup:
                 elements['properties'][e]['notes'] = element['Notes']
             if 'Required' in element:
                 required.append(e)
-            if required:
-                elements['required'] = required
-            elements['additionalProperties'] = False
+        if required:
+            elements['required'] = required
+        elements['additionalProperties'] = False
 
         return {group_name : elements}
 
@@ -43,20 +44,25 @@ class DataGroup:
             # If the type is an array, extract the surrounding [] first (using non-greedy qualifier "?")
             m = re.findall(r'\[(.*?)\]', parent_dict['Data Type'])
             if m:
+                # 1. 'type' entry
                 target_dict['type'] = 'array'
-                target_dict['items'] = dict()
-                k, v = self._get_simple_type(m[0])
-                target_dict['items'][k] = v
-                if 'Range' in parent_dict:
-                    self._get_simple_minmax(parent_dict['Range'], target_dict['items'])
+                # 2. 'm[in/ax]Items' entry
                 if len(m) > 1:
                     target_dict['minItems'] = int(m[1])
                     target_dict['maxItems'] = int(m[1])
                 else:
                     target_dict['minItems'] = 1
+                # 3. 'items' entry
+                target_dict['items'] = dict()
+                k, v = self._get_simple_type(m[0])
+                target_dict['items'][k] = v
+                if 'Range' in parent_dict:
+                    self._get_simple_minmax(parent_dict['Range'], target_dict['items'])
             else:
                 k, v = self._get_simple_type(parent_dict['Data Type'])
+                # 1. 'type' entry
                 target_dict[k] = v
+                # 2. 'm[in/ax]imum' entry
                 self._get_simple_minmax(parent_dict['Range'], target_dict)
         except KeyError as ke:
             #print('KeyError; no key exists called', ke)
@@ -64,26 +70,25 @@ class DataGroup:
 
 
     def _get_simple_type(self, type_str):
-        enum_or_def = r'(\{|\<)(.*)(\}|\>)'
+        ''' Return the internal type described by type_str, along with its json-appropriate key.
 
+            First, attempt to capture enum, definition, or special string type as references; 
+            then default to fundamental types with simple key "type". 
+        '''
+        enum_or_def = r'(\{|\<)(.*)(\}|\>)'
         m = re.match(enum_or_def, type_str)
         if m:
             internal_type = m.group(2)
-            for key in self._refs:
-                if internal_type in self._refs[key]:
-                    internal_type = key + '.schema.json#/definitions/' + internal_type
-            return ('$ref', internal_type)
-
-        if type_str == 'String':
-            return ('type', 'string')
-        elif type_str == 'Numeric':
-            return ('type', 'number')
-        elif type_str == 'Integer':
-            return ('type', 'integer')
-        elif type_str == 'Boolean':
-            return ('type', 'boolean')
         else:
-            # Validation error
+            internal_type = type_str
+        for key in self._refs:
+            if internal_type in self._refs[key]:
+                internal_type = key + '.schema.json#/definitions/' + internal_type
+                return ('$ref', internal_type)
+
+        try:
+            return ('type', self._types[type_str])
+        except KeyError:
             print('Type not processed:', type_str)
             return (None, None)
 
@@ -93,6 +98,8 @@ class DataGroup:
             ranges = range_str.split(',')
             minimum=None
             maximum=None
+            if 'type' not in target_dict:
+                target_dict['type'] = None
             for r in ranges:
                 try:
                     numerical_value = re.findall(r'\d+', r)[0]
@@ -144,7 +151,8 @@ class JSON_translator:
                         'description': None,
                         'definitions' : dict()}
         self._references = dict()
-        self._datatypes = list()
+        self._fundamental_data_types = dict()
+        self._specific_string_data_types = list()
 
 
     def load_metaschema(self, input_rs):
@@ -159,26 +167,19 @@ class JSON_translator:
                 obj_type = self._contents[base_level_tag]['Object Type']
                 if obj_type == 'Meta':
                     self._load_meta_info(self._contents[base_level_tag])
-                if obj_type == 'Data Type' and 'Is Regex' in self._contents[base_level_tag]:
-                    sch = {**sch, **({base_level_tag : {"type":"string", "regex":True}})}
-                elif obj_type == 'Data Type':
-                    pass
+                if obj_type == 'Data Type':
+                    self._load_data_type_info(self._contents[base_level_tag])
+                if obj_type == 'String Type':
+                    if 'Is Regex' in self._contents[base_level_tag]:
+                        sch = {**sch, **({base_level_tag : {"type":"string", "regex":True}})}
+                    #self._references[self._input_rs].append(base_level_tag)
                 if obj_type == 'Enumeration':
                     sch = {**sch, **(self._process_enumeration(base_level_tag))}
-                if obj_type == 'Data Group':
-                    dg = DataGroup(base_level_tag, self._references)
-                    sch = {**sch, **(dg.add_data_group(base_level_tag, 
-                                     self._contents[base_level_tag]['Data Elements']))}
-                if obj_type == 'Performance Map':
-                    dg = DataGroup(base_level_tag, self._references)
-                    sch = {**sch, **(dg.add_data_group(base_level_tag, 
-                                     self._contents[base_level_tag]['Data Elements']))}
-                if obj_type == 'Grid Variables':
-                    dg = DataGroup(base_level_tag, self._references)
-                    sch = {**sch, **(dg.add_data_group(base_level_tag, 
-                                     self._contents[base_level_tag]['Data Elements']))}
-                if obj_type == 'Lookup Variables':
-                    dg = DataGroup(base_level_tag, self._references)
+                if (obj_type == 'Data Group' or
+                    obj_type == 'Performance Map' or 
+                    obj_type == 'Grid Variables' or
+                    obj_type == 'Lookup Variables'):
+                    dg = DataGroup(base_level_tag, self._fundamental_data_types, self._references)
                     sch = {**sch, **(dg.add_data_group(base_level_tag, 
                                      self._contents[base_level_tag]['Data Elements']))}
         self._schema['definitions'] = sch
@@ -195,9 +196,16 @@ class JSON_translator:
             for ref_file in schema_section['References']:
                 ext_dict = load(os.path.join('..', 'schema-205', 'src', ref_file + '.schema.yaml'))
                 external_objects = list()
-                for base_item in ext_dict:
+                for base_item in [name for name in ext_dict if ext_dict[name]['Object Type'] in ['Enumeration', 'Data Group', 'String Type', 'Grid Variables', 'Lookup Variables', 'Performance Map']]:
                     external_objects.append(base_item)
                 self._references[ref_file] = external_objects
+                fun_types = list()
+                for base_item in [name for name in ext_dict if ext_dict[name]['Object Type'] == 'Data Type']:
+                    self._fundamental_data_types[base_item] = ext_dict[base_item]['JSON Schema Type']
+
+
+    def _load_data_type_info(self, schema_section):
+        return
 
 
     def _process_enumeration(self, name_key):
@@ -220,7 +228,7 @@ class JSON_translator:
 # -------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     j = JSON_translator()
-    sch = j.load_metaschema('ASHRAE205')
+    sch = j.load_metaschema(sys.argv[1])
     dump(sch, 'out.json')
 
 
