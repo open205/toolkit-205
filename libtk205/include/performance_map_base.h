fe3e0c596526ce8e5440d4d701e5df4d23b1f62c
tanaya-bigladder <57679220+tanaya-bigladder@users.noreply.github.com>
2020-12-08 12:48:01 -0700

    Remove hard-coded factory files; add autogeneration into build directory.

--- libtk205/include/performance_map_base.h
+++ libtk205/include/performance_map_base.h
@@ -18,26 +18,60 @@ public:
     performance_map_base(const performance_map_base& other) = default;
     performance_map_base& operator=(const performance_map_base& other) = default;
 
+  // ----------------------------------------------------------------------------------------------
+  /// @brief	
+  /// @param	j
+  // ----------------------------------------------------------------------------------------------
     virtual void Initialize(const nlohmann::json& j) = 0;
 
+  // ----------------------------------------------------------------------------------------------
+  /// @brief	
+  /// @param	axis TBD
+  // ----------------------------------------------------------------------------------------------
     inline void Add_grid_axis(std::vector<double>& axis) {
         _grid_axes.emplace_back(Btwxt::GridAxis(axis));
     }
 
+  // ----------------------------------------------------------------------------------------------
+  /// @brief	
+  /// @param	axis TBD
+  // ----------------------------------------------------------------------------------------------
     inline void Add_grid_axis(std::vector<int>& axis) {
         _grid_axes.emplace_back(Btwxt::GridAxis(std::vector<double>(axis.begin(), axis.end())));
     }
 
+  // ----------------------------------------------------------------------------------------------
+  /// @brief	
+  /// @param	table TBD
+  // ----------------------------------------------------------------------------------------------
     inline void Add_data_table(std::vector<double>& table) {
         _btwxt.add_value_table(table);
     }
     
+  // ----------------------------------------------------------------------------------------------
+  /// @brief	
+  // ----------------------------------------------------------------------------------------------
     inline void Finalize_grid() {
         auto gd = Btwxt::GriddedData(_grid_axes);
         _btwxt = Btwxt::RegularGridInterpolator(gd);
     }
 
-    inline void Calculate_performance(std::vector<double>& target);
+  // ----------------------------------------------------------------------------------------------
+  /// @brief	
+  /// @param	table_index TBD
+  // ----------------------------------------------------------------------------------------------
+    inline double Calculate_performance(std::size_t table_index) {
+        return _btwxt.get_value_at_target(table_index);
+    }
+
+  // ----------------------------------------------------------------------------------------------
+  /// @brief	Using pre-populated grid axes and lookup tables, calculate a set of performance
+  ///         results.
+  /// @param	target 
+  // ----------------------------------------------------------------------------------------------
+    inline std::vector<double> Calculate_performance(const std::vector<double> &target) {
+        return _btwxt.get_values_at_target(target);
+    }
 
 private:
     Btwxt::RegularGridInterpolator _btwxt;
