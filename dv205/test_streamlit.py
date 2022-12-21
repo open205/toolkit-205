418dd939ca048e9486047b7065f7cc2ef64d3ab1
Ralph Muehleisen <rmuehleisen@anl.gov>
2022-12-21 16:38:10 -0600

    Create test_streamlit.py
    Create file to test streamlit

---
+++ dv205/test_streamlit.py
@@ -0,0 +1,18 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
