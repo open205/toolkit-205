968783fd2772b34f246a4ead06955786a33ab510
Tanaya Mankad <tanaya.mankad@bigladdersoftware.com>
2023-05-16 16:06:11 -0600

    Clean up commented code.

--- cmake/update_lib_repo_local.cmake
+++ cmake/update_lib_repo_local.cmake
@@ -1,62 +1,5 @@
-# # repo_name is unique to the project
 set(repo_name "libtk205")
-# set(upload_repo "https://github.com/open205/${repo_name}")
-# set(authenticated_repo ${upload_repo})
-
-# message(STATUS ${authenticated_repo})
-
 set(clone_dir "${PROJECT_SOURCE_DIR}/${repo_name}")
-
-# find_package(Git QUIET)
-
-# if(GIT_FOUND)
-#    if(NOT EXISTS "${clone_dir}")
-#       execute_process(COMMAND ${GIT_EXECUTABLE} clone ${authenticated_repo}
-#                       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-#                       RESULT_VARIABLE GIT_SUBMOD_RESULT)
-#       if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-#          message(FATAL_ERROR "${GIT_EXECUTABLE} clone ${authenticated_repo} failed with ${GIT_SUBMOD_RESULT}.")
-#       endif()
-#    # else() pull or fetch?
-#    endif()
-# else()
-#    message(FATAL_ERROR "git not found!")
-# endif()
-
-# # git branch name query
-# execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
-#                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-#                 OUTPUT_VARIABLE current_git_branch
-#                 OUTPUT_STRIP_TRAILING_WHITESPACE
-# )
-# message(STATUS "Current local branch is ${current_git_branch}.")
-
-# execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --pretty=%B
-#                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-#                 OUTPUT_VARIABLE last_commit_msg
-#                 OUTPUT_STRIP_TRAILING_WHITESPACE
-# )
-# message(STATUS "Last commit message on local branch was \"${last_commit_msg}\"")
-
-# execute_process(COMMAND ${GIT_EXECUTABLE} ls-remote --heads --exit-code ${authenticated_repo} ${current_git_branch}
-#                 WORKING_DIRECTORY ${clone_dir}
-#                 RESULT_VARIABLE exit_code
-#                 OUTPUT_VARIABLE output_repo_branches
-# )
-# if(exit_code EQUAL "0")
-#    message(STATUS "Branch ${current_git_branch} exists in upload remote.")
-#    execute_process(COMMAND ${GIT_EXECUTABLE} checkout ${current_git_branch}
-#                    WORKING_DIRECTORY ${clone_dir}
-#    )
-# else()
-#    message(STATUS "Branch ${current_git_branch} must be created in upload remote.")
-#    execute_process(COMMAND ${GIT_EXECUTABLE} checkout -b ${current_git_branch}
-#                    WORKING_DIRECTORY ${clone_dir}
-#    )
-# #    execute_process(COMMAND ${GIT_EXECUTABLE} push -u origin ${current_git_branch}
-# #                    WORKING_DIRECTORY ${clone_dir}
-# #    )
-# endif()
 
 set(include_dest_dir "${clone_dir}/include/${repo_name}")
 set(src_dest_dir "${clone_dir}/src")
@@ -72,10 +15,6 @@ foreach(filepath ${generated_rs_headers})
   file(RENAME ${filepath} ${include_dest_dir}/${filename})
 endforeach(filepath)
 
-# file(COPY "${PROJECT_SOURCE_DIR}/include/" # No longer copied from toolkit; exist already in libtk205 repo
-#      DESTINATION "${include_dest_dir}"
-#      FILES_MATCHING PATTERN *.h)
-
 file(COPY "${PROJECT_SOURCE_DIR}/../schema-205/schema205/libtk205_fixed_src/include/"
      DESTINATION "${include_dest_dir}"
      FILES_MATCHING PATTERN *.h)
@@ -87,19 +26,9 @@ file(GLOB generated_rs_src "${PROJECT_SOURCE_DIR}/schema-205/build/cpp/*.cpp")
        file(RENAME ${filepath} ${src_dest_dir}/${filename})
      endforeach(filepath)
      
-# file(COPY "${PROJECT_SOURCE_DIR}/src/" # doesn't exist
-#      DESTINATION "${src_dest_dir}"
-#      FILES_MATCHING PATTERN *.cpp)
-
 file(COPY "${PROJECT_SOURCE_DIR}/../schema-205/schema205/libtk205_fixed_src/src/"
      DESTINATION "${src_dest_dir}"
      FILES_MATCHING PATTERN *.cpp)
-
-# file(COPY "${PROJECT_SOURCE_DIR}/test/" # No longer copied from toolkit; exist already in libtk205 repo
-#      DESTINATION "${clone_dir}/test"
-#      FILES_MATCHING 
-#       PATTERN *.cpp
-#       PATTERN *.hpp)
 
 file(COPY "${PROJECT_SOURCE_DIR}/schema-205/examples"
      DESTINATION "${clone_dir}")
